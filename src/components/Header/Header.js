d57ea4d2bc4b9ff93f341da433f4d4ff64fa8db5
ezmionline <edward@ezmi.co.uk>
2015-12-06 16:05:50 +0000

    remove navigation component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,6 @@ import React, { Component } from 'react';
 import styles from './Header.scss';
 import withStyles from '../../decorators/withStyles';
 import Link from '../Link';
-import Navigation from '../Navigation';
 
 @withStyles(styles)
 class Header extends Component {
