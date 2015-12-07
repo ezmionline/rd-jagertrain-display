200c491d51a185fade6d9beca42e73903db021c0
ezmionline <edward@ezmi.co.uk>
2015-12-07 23:18:05 +0000

    fetch total stops from firebase transactions

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,11 +4,28 @@ import React, { PropTypes, Component } from 'react';
 import styles from './Footer.scss';
 import withViewport from '../../decorators/withViewport';
 import withStyles from '../../decorators/withStyles';
-import Link from '../Link';
+import Firebase from 'firebase';
+import _ from 'lodash';
 
 @withViewport
 @withStyles(styles)
 class Footer extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      totalStops: 0
+    }
+
+    this.firebaseRef = new Firebase('https://jagertrain.firebaseio.com/transactions');
+    this.firebaseRef.once("value", (dataSnapshot)=> {
+      var transactions = dataSnapshot.val();
+      this.setState({
+        totalStops: _.size(transactions)
+      });
+    });
+  }
 
   static propTypes = {
     viewport: PropTypes.shape({
@@ -22,6 +39,7 @@ class Footer extends Component {
       <footer className="Footer">
         <img className="Footer-jagertrain" src={require('./jagertrain.png')} alt="Riskdisk Jagertrain - On it, till we vomit!" />
         <h3>It’s Jager Time!!! Please Drink Irresponsibly #RDJT</h3>
+        <h3 className="Footer-counter">Total Stops: <span>{this.state.totalStops}</span></h3>
       </footer>
     );
   }
